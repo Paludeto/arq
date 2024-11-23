bd710eac37ed5130be37fe961405614e4042c666
Paludeto <gpalu54@gmail.com>
2024-11-23 20:19:21 -0300

    ex 2 loadstore

--- load_store/loadstore_2.s
+++ load_store/loadstore_2.s
@@ -12,43 +12,50 @@
 		
 	# $s0 armazenará o nosso índice, $s9 o limite
 		li $s0, 0
-		li $s1, 9
-		
-		bne $s0, $s1, forLoop
-	
-		j sair
+		li $s1, 10
 	
 	forLoop:
 	
+	# Verifica condição de saída do loop
+		beq $s0, $s1, sair
+
 	# Soma de elementos
 		lw $s2, 0($t0)
 		addi $s2, $s2, 15
+		sw $s2, 0($t0)
 		
-	# Incrementa $t0 em 4 unidades (offset) e $s0 em 1 unidade
-		addi $t0, $t0, 4
-		addi $s0, $s0, 1
-		
-	# Syscalls strings
+	# Exibição de "Vetor [" 
 		la $a0, string1
 		li $v0, 4
 		syscall
 		
-	# Move $s2 para $a0 e dá uma syscall para exibir o novo valor
-		move $a0, $s2
+	# Mostra índice ($s0)
+		move $a0, $s0
 		li $v0, 1
 		syscall
-		
+	
+	# Exibe strings "] = "
 		la $a0, string2
 		li $v0, 4
 		syscall
 		
+
+	# Mostra valor atualizado
+		move $a0, $s2
+		li $v0, 1
+		syscall
+	
+	# Mostra string "\n
 		la $a0, string3
 		li $v0, 4
 		syscall
+
+	# Incrementa $t0 em 4 unidades (offset) e $s0 em 1 unidade (índice)
+		addi $t0, $t0, 4
+		addi $s0, $s0, 1
 		
-	# Volta para main
-		
-		j main
+	# Volta para o ínicio do loop
+		j forLoop
 	
 	sair: 
 		li $v0, 10
